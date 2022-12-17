c246901e5c87c39b748850521f9554010636ee3b
Mikhail <mishaserebryakov.02@gmail.com>
2022-12-17 23:03:25 +0600

    восьмая задача

---
+++ Dz1(8)/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+Console.WriteLine("Введите число:"); 
+
+double Num = Convert.ToDouble(Console.ReadLine());
+
+double chet = 0;
+int i;
+Console.Write("Чётные числа до числа " + Num + ": "); 
+for ( i = 1; i < Num; i++ )
+{
+chet = 0 + i;
+
+if ( chet % 2 == 0 )
+{
+
+Console.Write( chet + " ");
+
+}
+
+}
+
+
+
