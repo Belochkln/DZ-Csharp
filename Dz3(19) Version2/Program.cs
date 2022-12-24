557aca1af73a901ba6a646b7c34beb3c702f88a9
Mikhail <mishaserebryakov.02@gmail.com>
2022-12-24 17:42:17 +0600

    19 задача

---
+++ Dz3(19) Version2/Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Введите пятизначное число: ");
+string number = Console.ReadLine();
+int i;
+
+if (Convert.ToInt32(number) > 0)
+{
+    if (number.Length == 5)
+    {
+        if (number[0] == number[4] && number[1] == number[3])
+            Console.WriteLine("Полиндром");
+        else
+            Console.WriteLine("Не Полиндром");
+    }
+    else
+        Console.WriteLine("Это не пятизначное число");
+}
+
+else
+{
+    if (number.Length == 5)
+    {
+        if (number[1] == number[5] && number[2] == number[4])
+            Console.WriteLine("Полиндром");
+        else
+            Console.WriteLine("Не Полиндром");
+    }
+    else
+        Console.WriteLine("Это не пятизначное число");
+}
