e27cff2f36bb65e2285599fa67bb15bf4f4743d0
Mikhail <mishaserebryakov.02@gmail.com>
2023-01-25 22:46:10 +0600

    задача 47

---
+++ Dz7(47)/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+Console.Write("Введите кол-во строк: ");
+int row = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите кол-во столбцов: ");
+int column = Convert.ToInt32(Console.ReadLine());
+
+Random randomNumber = new Random();
+double[,] table = new double[row, column];
+Console.WriteLine("Полученный массив: ");
+
+for (int i = 0; i < row; i++)
+{
+    for (int j = 0; j < column; j++)
+    {
+        table[i, j] = Math.Round((randomNumber.NextDouble() * randomNumber.Next(-100, 100)), 1);
+        Console.Write(table[i, j] + "  ");
+    }
+    Console.WriteLine();
+}
