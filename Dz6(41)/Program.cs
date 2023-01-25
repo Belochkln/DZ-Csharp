e65e8154cb12bd6d12cd7a5de940b8e34cb1e84b
Mikhail <mishaserebryakov.02@gmail.com>
2023-01-25 12:43:04 +0600

    задача 38

---
+++ Dz6(41)/Program.cs
@@ -0,0 +1,11 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+Console.Write("Введите числа(через пробел): ");
+double[] numbers = Array.ConvertAll(Console.ReadLine().Split(), Convert.ToDouble);
+int amount = 0;
+
+for (int i = 0; i < numbers.Length; i++)
+{
+    if(numbers[i] > 0) amount = amount + 1;
+}
+
+Console.WriteLine("Количество положительных чисел: " + amount);
