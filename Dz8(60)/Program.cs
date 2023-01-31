a573ab883cd7e27b7afc835d0acca0ecc8139d7f
Mikhail <mishaserebryakov.02@gmail.com>
2023-01-31 19:52:39 +0600

    задача 60

---
+++ Dz8(60)/Program.cs
@@ -0,0 +1,51 @@
+﻿// Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. Напишите программу, которая будет построчно выводить массив, добавляя индексы каждого элемента.
+int row = new Random().Next(2, 2);
+int column = new Random().Next(2, 2);
+int deep = new Random().Next(2, 2);
+
+bool CompareAllElements(int[,,] array, int element)
+{
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < column; j++)
+        {
+            for (int k = 0; k < deep; k++)
+            {
+                if (array[i, j, k] == element) return true;
+            }
+        }
+    }
+    return false;
+}
+
+Console.WriteLine("Полученный массив: ");
+int[,,] Filltable(int[] array)
+{
+    int[,,] fillTable = new int[row, column, deep];
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < column; j++)
+        {
+            for (int k = 0; k < deep; k++)
+            {
+                int tableElement = new Random().Next(10, 20);
+                if (CompareAllElements(fillTable, tableElement)) continue;
+                fillTable[i, j, k] = tableElement;
+            }
+        }
+    }
+    return fillTable;
+}
+
+int[,,] table = Filltable(new int[] { row, column, deep });
+for (int i = 0; i < row; i++)
+{
+    for (int j = 0; j < column; j++)
+    {
+        for (int k = 0; k < deep; k++)
+        {
+            Console.Write($"{table[i, j, k]}({i},{j},{k}) ");
+        }
+        Console.WriteLine();
+    }
+}
