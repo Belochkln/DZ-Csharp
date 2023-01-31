899c6162083dc40e1041703f9a896d621266e993
Mikhail <mishaserebryakov.02@gmail.com>
2023-01-31 18:53:22 +0600

    задача 58 испр

--- Dz8(58)/Program.cs
+++ Dz8(58)/Program.cs
@@ -1,43 +1,58 @@
 ﻿//Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц.
-int matrix = new Random().Next(2, 5);
 
-Random randomNumber = new Random();
-int[,] tableOne = new int[matrix, matrix];
-int[,] tableTwo = new int[matrix, matrix];
+Console.WriteLine("Задайте две матрицы.");
+Console.WriteLine("Задайте кол-во строк 1-ой матрицы матрицы: ");
+int row1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Задайте кол-во строк 2-ой матрицы матрицы: ");
+int row2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Задайте кол-во столбов 1-ой матрицы матрицы: ");
+int column1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Задайте кол-во столбов 2-ой матрицы матрицы: ");
+int column2 = Convert.ToInt32(Console.ReadLine());
 
-Console.WriteLine("Заданы две матрицы: ");
-
-for (int i = 0; i < matrix; i++)
+if (column1 != row2) Console.WriteLine("Эти матрицы не перемножить.");
+else
 {
-    for (int j = 0; j < matrix; j++)
-    {
-        tableOne[i, j] = new Random().Next(10, 100);
-        Console.Write(tableOne[i, j] + "  ");
-    }
-    Console.Write(" | ");
+    Console.WriteLine("Полученные матрицы:");
+    int[,] tableOne = new int[row1, column1];
+    int[,] tableTwo = new int[row2, column2];
 
-    for (int j = 0; j < matrix; j++)
+    Console.WriteLine("Первая матрица:");
+    for (int i = 0; i < row1; i++)
     {
-        tableTwo[i, j] = new Random().Next(10, 100);
-        Console.Write(tableTwo[i, j] + "  ");
-    }
-    Console.WriteLine();
-}
-
-Console.WriteLine("Произведение этих двух матриц: ");
-int[,] resultMatrix = new int[matrix, matrix];
-
-for (int i = 0; i < matrix; i++)
-{
-    for (int k = 0; k < matrix; k++)
-    {
-        int result = 0;
-        for (int j = 0; j < matrix; j++)
+        for (int j = 0; j < column1; j++)
         {
-            result = result + (tableOne[i, j] * tableTwo[j, k]);
+            tableOne[i, j] = new Random().Next(10, 100);
+            Console.Write(tableOne[i, j] + "  ");
         }
-        resultMatrix[i, k] = result;
-        Console.Write(resultMatrix[i, k] + " ");
+        Console.WriteLine(" ");
     }
-    Console.WriteLine();
+    Console.WriteLine("Вторая матрица:");
+    for (int i = 0; i < row2; i++)
+    {
+        for (int j = 0; j < column2; j++)
+        {
+            tableTwo[i, j] = new Random().Next(10, 100);
+            Console.Write(tableTwo[i, j] + "  ");
+        }
+        Console.WriteLine(" ");
+    }
+
+    Console.WriteLine("Произведение этих двух матриц: ");
+    int[,] resultMatrix = new int[row2, column1];
+
+    for (int i = 0; i < row1; i++)
+    {
+        for (int k = 0; k < column2; k++)
+        {
+            int result = 0;
+            for (int j = 0; j < column1; j++)
+            {
+                result = result + (tableOne[i, j] * tableTwo[j, k]);
+            }
+            resultMatrix[i, k] = result;
+            Console.Write($"{resultMatrix[i, k]}  ");
+        }
+        Console.WriteLine();
+    }
 }
