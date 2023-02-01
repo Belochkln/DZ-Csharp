bebd8999afe6a7bd35116143e9f41faedc628dd1
Mikhail <mishaserebryakov.02@gmail.com>
2023-02-02 01:29:33 +0600

    задача 64

---
+++ Dz9(64)/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.Read());
+
+string ShowNumbers(int N)
+{
+    string result = String.Empty;
+    for (int i = (N - 48); i > 0; i--)
+    {
+        result += $"{i} ";
+    }
+    return result;
+}
+Console.WriteLine("Все натуральные числа в промежутке от " + (number - 48) + " до 1: ");
+Console.WriteLine(ShowNumbers(number));
